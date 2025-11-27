2b7d7095221e685a2d37168e35ea3fd17c4bb457
keenan21109 <keenanscully21@gmail.com>
2025-11-27 23:00:48 +0200

    Add files via upload

---
+++ Main/src/test/java/com/example/studentmanagement/StudentTest.java
@@ -0,0 +1,139 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.example.studentmanagement;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Keenan
+ */
+public class StudentTest {
+    
+    public StudentTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of getStudentId method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetStudentId() {
+        System.out.println("getStudentId");
+        Student instance = null;
+        String expResult = "";
+        String result = instance.getStudentId();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getName method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetName() {
+        System.out.println("getName");
+        Student instance = null;
+        String expResult = "";
+        String result = instance.getName();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getEmail method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetEmail() {
+        System.out.println("getEmail");
+        Student instance = null;
+        String expResult = "";
+        String result = instance.getEmail();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getCourse method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetCourse() {
+        System.out.println("getCourse");
+        Student instance = null;
+        String expResult = "";
+        String result = instance.getCourse();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of getAge method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testGetAge() {
+        System.out.println("getAge");
+        Student instance = null;
+        int expResult = 0;
+        int result = instance.getAge();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of toString method, of class Student.
+     */
+    @org.junit.jupiter.api.Test
+    public void testToString() {
+        System.out.println("toString");
+        Student instance = null;
+        String expResult = "";
+        String result = instance.toString();
+        assertEquals(expResult, result);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
